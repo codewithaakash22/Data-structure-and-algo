180a8f8b163f8ce8007698adc3cdf1e8d5668ffc
codewithaakash22 <webbyaakasah01@gmail.com>
2024-03-19 22:54:02 +0530

    Adding comments

--- BinaryTree_linked_representation.c
+++ BinaryTree_linked_representation.c
@@ -7,30 +7,53 @@ struct Node{
 };
 
 struct Node* CreateNode(int data){
-    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
-    n->data = data;
-    n->left = NULL;
-    n->right = NULL;
-    return n;
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));  // Creating a node pointer
+    n->data = data;       // Setting the data 
+    n->left = NULL;      // setting the left and right children to NULL
+    n->right = NULL;    // setting the left and right children to NULL
+    return n;          // Finally returning the created node.
 }
 
 void PreOrder(struct Node* root){
-    if(root != NULL){
-       printf("%d ",root->data);
+    if(root != NULL){            
+       printf("%d ",root->data); 
        PreOrder(root->left);
        PreOrder(root->right);
     }
 }
 
 void main(){
+
+            /*
+            //constructing thre Root Node
+            struct Node* p = (struct Node*)malloc(sizeof(struct Node));
+            p->data = 12;
+            p->left = NULL;
+            p->right = NULL;
+
+            //constructing the Second Node
+            struct Node* p1 = (struct Node*)malloc(sizeof(struct Node));
+            p1->data = 24;
+            p1->left = NULL;
+            p1->right = NULL;
+
+            //constructing the  Third Node
+            struct Node* p2 = (struct Node*)malloc(sizeof(struct Node));
+            p2->data = 36;
+            p2->left = NULL;
+            p2->right = NULL;
+
+            */
+
             struct Node* p = CreateNode(12);
             struct Node* p1 = CreateNode(24);
             struct Node* p2 = CreateNode(36);
 
 
+            //Linking the root node with left and right children 
             p->left = p1;
             p->right = p2;
 
-            PreOrder(p1);
+            PreOrder(p);
             
 }
