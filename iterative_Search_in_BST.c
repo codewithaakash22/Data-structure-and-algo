dbeac704fb4d619c44549f8949572b2ed857bbea
codewithaakash22 <webbyaakasah01@gmail.com>
2024-03-24 14:11:26 +0530

    feat: iterative search in BST

---
+++ iterative_Search_in_BST.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<malloc.h>
+struct Node{
+                int data;
+                struct Node* left;
+                struct Node* right;
+};
+
+struct Node* createNode(int val){
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node)); 
+    n->data = val; 
+    n->left = NULL; 
+    n->right = NULL;
+
+    return n;
+}
+
+void inOrder(struct Node* root){
+    if(root!=NULL){
+        inOrder(root->left);
+        printf("%d ",root->data);
+        inOrder(root->right);
+    }
+}
+
+int isBST(struct Node* root){
+    static struct Node* prev = NULL;
+    if(root!= NULL){
+        if(!isBST(root->left)){
+            return 0;
+        }
+        if(prev != NULL && root->data <= prev->data){
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else{
+        return 1;
+    }
+}
+
+struct Node* IterSearch(struct Node* root, int key){
+    while(root != NULL){ 
+    if(root->data == key){
+        return root;
+    }
+    else if(key < root->data){
+        root = root->left;
+    }
+    else{
+        root = root->right;
+    }
+    }
+    return NULL;
+}
+
+
+void main(){
+                struct Node* p = createNode(5);
+                struct Node* p1 = createNode(2);
+                struct Node* p2 = createNode(7);
+                struct Node* p3 = createNode(1);
+                struct Node* p4 = createNode(3);
+                struct Node* p5 = createNode(6);
+                struct Node* p6 = createNode(9);
+
+
+                p->left = p1;
+                p->right = p2;
+
+                p1->left = p3;
+                p1->right = p4;
+
+                p2->left = p5;
+                p2->right = p6;
+
+
+                inOrder(p);
+                
+                struct Node* n = IterSearch(p,1);
+                
+                if(n != NULL){
+                    printf("\nElement %d found\n",n->data);
+                }
+                else{
+                    printf("\nElement not found\n");
+                }
+}
